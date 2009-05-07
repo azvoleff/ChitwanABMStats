bf6f7da343866605b2e51afaeb28dab29070e19b
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2009-05-07 13:50:11 -0700

    Update hazard of death scripts.
    Added new get_deaths.R file to load the deaths from the HH registry data and
    save them to an Rdata file for later use.

---
+++ HazardDeath/get_deaths.R
@@ -0,0 +1,66 @@
+###############################################################################
+# Loads deaths and age at death from the ICPSR Restricted dataset DS0010, the 
+# household registry data.
+###############################################################################
+
+require("foreign")
+
+hhreg <- read.xport("/media/Restricted/Data/ICPSR_0538_Restricted/da04538-0010_REST.xpt")
+# A death is coded in the household registry data as a 3 in LIVNG1-LIVNG54. Any 
+# other value indicates that the individual was alive.
+# LIVNG1 - LIVNG54 are indexed as columns 117-170 in the dataframe.
+# AGE1 - AGE54 are indexed as columns 7-60 in the dataframe.
+livngs <- hhreg[117:170]
+ages <- hhreg[7:60]
+
+# Here I will recode the data so any point where an individual was known alive 
+# is a 2, the period in which they died is a 3, and all other times (including 
+# when they were away (alive/dead unknown) is a 1.
+livngs[livngs==4] <- 2 # Code new HH member as alive
+livngs[livngs==5] <- 2 # Code HH merged as alive
+livngs[livngs==6] <- 1 # Code away from HH as unknown
+livngs[is.na(livngs)] <- 1 # Code NA as unknown
+
+# Now, for each individual, find their age at death, or the age at which they 
+# were last known to be alive. Do this by creating a vector of length 
+# equivalent to the number of individuals in the dataset, and by then iterating 
+# over the months, and updating it with each known alive or known dead event.  
+status <- rep(NA, nrow(hhreg))
+statusage <- rep(NA, nrow(hhreg))
+statustime <- rep(NA, nrow(hhreg))
+for (month in 1:54) {
+    age <- ages[,month]
+    # First code the new deaths
+    index <- livngs[,month]
+    index[index!=3] <- 0 # This zero indicates this position WILL NOT be changed
+    index[index==3] <- 1 # This zero indicates this position WILL be changed
+    index <- as.logical(index)
+    status[index] <- 1
+    statusage[index] <- age[index]
+    statustime[index] <- month
+    # Now code the new "known-alives"
+    index <- livngs[,month]
+    index[index!=2] <- 0 # This zero indicates this position WILL NOT be changed
+    index[index==2] <- 1 # This zero indicates this position WILL be changed
+    index <- as.logical(index)
+    status[index] <- 0
+    statusage[index] <- age[index]
+    statustime[index] <- month
+}
+status <- na.omit(status)
+statusage <- na.omit(statusage)
+statustime <- na.omit(statustime)
+
+# Count the deaths within each group
+bin.breakpoints <- c(0, 3, 6, 12, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+bin.yvalues<- rep(NA, length(bin.breakpoints)-1)
+ndeaths <- rep(NA, length(bin.breakpoints)-1)
+nmembers <- rep(NA, length(bin.breakpoints)-1)
+for (n in 1:length(bin.breakpoints)-1) {
+    agegroup.members.indices <- (statusage >= bin.breakpoints[n]) & (statusage < bin.breakpoints[n+1])
+    agegroup.deaths.indices <- (status == 1) & agegroup.members.indices
+    ndeaths[n] <- length(status[agegroup.deaths.indices])
+    nmembers[n] <- length(status[agegroup.members.indices])
+}
+
+save(bin.breakpoints, ndeaths, nmembers, status, statusage, statustime, file="chitwan_deaths.Rdata")
--- HazardDeath/hazard_death.R
+++ HazardDeath/hazard_death.R
@@ -1,55 +1,24 @@
 ###############################################################################
-# Creates a survival model to calculate the hazard of death at a range of ages, 
-# to be used in the calibration of the Chitwan Valley ABM.
+# Creates a series of models to calculate the hazard of death at a range of 
+# ages, to be used in the calibration of the Chitwan Valley ABM.
 ###############################################################################
 
-require("foreign")
 require("survival")
+load("chitwan_deaths.Rdata")
 
-hhreg <- read.xport("/media/Restricted/Data/ICPSR_0538_Restricted/da04538-0010_REST.xpt")
+pdf()
+par(mfrow=c(2,2))
 
-# A death is coded in the household registry data as a 3 in LIVNG1-LIVNG54. Any 
-# other value indicates that the individual was alive.
-# LIVNG1 - LIVNG54 are indexed as columns 117-170 in the dataframe.
-# AGE1 - AGE54 are indexed as columns 7-60 in the dataframe.
-livngs <- hhreg[117:170]
-ages <- hhreg[7:60]
+# Divide the hazards by 54/12 to convert them to be yearly hazards of death.
+hazard.death <- (ndeaths/nmembers) / (54/12)
+plot(bin.breakpoints[-1], hazard.death, type="s", xlab="Age (years)", ylab="Hazard of death", main="Chitwan valley death hazard")
 
-# Here I will recode the data so any point where an individual was known alive 
-# is a 2, the period in which they died is a 3, and all other times (including 
-# when they were away (alive/dead unknown) is a 1.
-livngs[livngs==4] <- 2 # Code new HH member as alive
-livngs[livngs==5] <- 2 # Code HH merged as alive
-livngs[livngs==6] <- 1 # Code away from HH as unknown
-livngs[is.na(livngs)] <- 1 # Code NA as unknown
+deaths <- Surv(statustime, status)
+plot(survfit(deaths), xlab="Time interval", ylab="Survivorship", main="Chitwan survival")
 
-# Now, for each individual, find their age at death, or the age at which they 
-# were last known to be alive. Do this by creating a vector of length 
-# equivalent to the number of individuals in the dataset, and by then iterating 
-# over the months, and updating it with each known alive or known dead event.  
-status <- matrix(NA, nrow(hhreg), 1)
-statusage <- matrix(NA, nrow(hhreg), 1)
-for (month in 1:54) {
-    print(month)
-    age <- ages[,month]
-    # First code the new deaths
-    index <- livngs[,month]
-    index[index!=3] <- 0 # This zero indicates this position WILL NOT be changed
-    index[index==3] <- 1 # This zero indicates this position WILL be changed
-    index <- as.logical(index)
-    status[index] <- 1
-    statusage[index] <- age[index]
-    # Now code the new "known-alives"
-    index <- livngs[,month]
-    index[index!=2] <- 0 # This zero indicates this position WILL NOT be changed
-    index[index==2] <- 1 # This zero indicates this position WILL be changed
-    index <- as.logical(index)
-    status[index] <- 0
-    statusage[index] <- age[index]
-    print(summary(status))
-}
-deaths <- Surv(statusage, status)
-plot(survfit(deaths), xlab="Age (years)", ylab="Survivorship", main="Chitwan mortality")
+model.1 <- survfit(deaths~statusage)
+plot(model, ylab="Survivorship", xlab="Age (years)")
 
-model <- survfit(deaths~statusage, dist="weibull")
-plot(model, lty=c(1,3), ylab="Survivorship", xlab="Age (years)")
+hist(statusage[status==1], breaks=20, xlab="Age at death (years)", main="Chitwan Valley, age at death")
+hist(statusage, breaks=20, xlab="Age of last observation (years)", main="Chitwan Valley, age at last known status")
+dev.off()
