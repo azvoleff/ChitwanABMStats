5842e5e210b4e261a7c97a968653c1c37ffdcedc
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2009-05-07 17:19:31 -0700

    Update migration hazard calculation.

--- HazardMigration/migrations_statistics.R
+++ HazardMigration/migrations_statistics.R
@@ -2,8 +2,9 @@
 # Runs logistics regressions to predict the hazard of migration from a set of 
 # variables using lmer, and glm.
 ###############################################################################
-library("foreign")
-library("lme4")
+require("foreign")
+require("lme4")
+require("MASS")
 
 # Load the migration data (stored in a dataframe as person-months)
 load("migrations_allvars.Rdata")
@@ -42,13 +43,16 @@ outcome[outcome!=1] <- 0
 #    (BAE2 + BAE25 + BAA43 + BAB4 | HHID), family=binomial))
 
 local_model <- with(migrations, glm(outcome ~ CENGENDR + SALYN + SCHL1996 + YRSSCHL + 
-    AGE1524 + AGE2534 + AGE3544 + TIME + TIME**2 + BAE2 + BAE25 + BAA43 + BAB4 + SEASON,
+    AGE1524 + AGE2534 + AGE3544 + TIME + TIME**2 + BAE2 + BAE25 + BAA43 + BAB4,
     family=binomial))
+results <- summary(local_model)$coefficients[,c(0,1,3)]
+results[,1] <- exp(results[,1])
+results <- round(results, digits=2)
+write.matrix(results, file="local_model.csv", sep=",")
 
 local_model_climate <- with(migrations, glm(outcome ~ CENGENDR + SALYN + SCHL1996 + YRSSCHL + 
     AGE1524 + AGE2534 + AGE3544 + TIME + TIME**2 + BAE2 + BAE25 + BAA43 + BAB4 + CLIMATE,
     family=binomial))
-
 results <- summary(local_model_climate)$coefficients[,c(1,3)]
 results[,1] <- exp(results[,1])
 results <- round(results, digits=2)
@@ -67,11 +71,14 @@ outcome[outcome==2] <- 1
 distant_model <- with(migrations, glm(outcome ~ CENGENDR + SALYN + SCHL1996 + YRSSCHL + 
     AGE1524 + AGE2534 + AGE3544 + TIME + TIME**2 + BAE2 + BAE25 + BAA43 + BAB4,
     family=binomial))
+results <- summary(distant_model)$coefficients[,c(0,1,3)]
+results[,1] <- exp(results[,1])
+results <- round(results, digits=2)
+write.matrix(results, file="distant_model.csv", sep=",")
 
 distant_model_climate <- with(migrations, glm(outcome ~ CENGENDR + SALYN + SCHL1996 + YRSSCHL + 
     AGE1524 + AGE2534 + AGE3544 + TIME + TIME**2 + BAE2 + BAE25 + BAA43 + BAB4 + CLIMATE,
     family=binomial))
-
 results <- summary(distant_model_climate)$coefficients[,c(0,1,3)]
 results[,1] <- exp(results[,1])
 results <- round(results, digits=2)
