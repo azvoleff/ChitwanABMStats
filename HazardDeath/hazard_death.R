3a8f2c8cfab4d02338e18b68042c9132f006161e
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2009-06-04 17:27:31 -0700

    Whitespace.

--- HazardDeath/hazard_death.R
+++ HazardDeath/hazard_death.R
@@ -10,20 +10,25 @@ load("chitwan_deaths.Rdata")
 pdf()
 par(mfrow=c(2,2))
 
-plot(deathhazard, type="s", xlab="Age (years)", ylab="Hazard of death", main="Chitwan valley death hazard")
+plot(deathhazard, type="s", xlab="Age (years)", ylab="Hazard of death",
+    main="Chitwan valley death hazard")
 
 deaths <- Surv(statustime, status)
-plot(survfit(deaths), xlab="Time interval", ylab="Survivorship", main="Chitwan survival")
+plot(survfit(deaths), xlab="Time interval", ylab="Survivorship",
+    main="Chitwan survival")
 
 model.1 <- survfit(deaths~statusage)
 plot(model, ylab="Survivorship", xlab="Age (years)")
 
-hist(statusage[status==1], breaks=20, xlab="Age at death (years)", main="Chitwan Valley, age at death")
-hist(statusage, breaks=20, xlab="Age of last observation (years)", main="Chitwan Valley, age at status")
+hist(statusage[status==1], breaks=20, xlab="Age at death (years)",
+    main="Chitwan Valley, age at death")
+hist(statusage, breaks=20, xlab="Age of last observation (years)",
+    main="Chitwan Valley, age at status")
 
 dev.off()
 
 # Save the death hazards to a text file, after converting the hazards to 
 # percentages
 deathhazard[,2] <- deathhazard[,2]*100
-write(t(deathhazard), file="death_hazard.txt", ncolumns=ncol(deathhazard), sep=",")
+write(t(deathhazard), file="death_hazard.txt", ncolumns=ncol(deathhazard),
+    sep=",")
