b64d6902b5805ab686f2648b9df860a97fd084cd
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2009-12-02 20:25:07 +0545

    Update code to use timeseries.

--- Migration/migration_count.R
+++ Migration/migration_count.R
@@ -11,7 +11,7 @@
 
 # MONTHS.AWAY gives the number of months a person must be away for a move to be 
 # considered a migration.
-MONTHS.AWAY <- 6
+MONTHS.AWAY <- 1
 
 library("Hmisc")
 #hhreg <- sasxport.get("/media/Restricted/Data/ICPSR_0538_Restricted/da04538-0010_REST.xpt")
@@ -32,22 +32,9 @@ place.cols <- grep('^place[0-9]*$', names(hhreg))
 places <- hhreg[place.cols]
 age.cols <- grep('^age[0-9]*$', names(hhreg))
 
-# Setup 'months' vector to use in plotting.  Months 1-126 correspond to 
-# February 1997 - June 2007.
-dates <- c(as.Date("2/1/1997", "%m/%d/%Y"))
-# The +2 below is because the dates start in March
-for (n in 3:(length(place.cols)+2)) {
-    year <- 1997 + floor((n - 1) / 12)
-    month <- (n - 1) %% 12 + 1
-    dates <- c(dates, as.Date(paste(month,"1", year, sep="/"),
-        "%m/%d/%Y"))
-}
-# Cut out months at end for which data is not collected for enough subsequent 
-# months to determine if a migration of at least MONTHS.AWAY ocurred
-dates <- dates[-c((length(dates)-MONTHS.AWAY-1):length(dates))]
 
-# This function provides a mask to only consider people who have been living in 
-# the same place for a certain number of months. That way, persons who 
+# This function provides a mask to only consider people who have been living 
+# in the same place for a certain number of months. That way, persons who 
 # "migrated" and then returned to the same place after a month can be 
 # discounted in the analysis.
 mig.mask<- function(places, num.months) {
@@ -71,6 +58,8 @@ mask.mig.duration <- mig.mask(places, MONTHS.AWAY)
 # month.
 mask.mig.duration <- mask.mig.duration[,-1]
 
+
+
 # Setup place0 and place1 
 place0.cols <- 1:(length(place.cols)-MONTHS.AWAY-1)
 place1.cols <- 2:(length(place.cols)-MONTHS.AWAY)
@@ -84,7 +73,9 @@ mig.all[mig.all<0] <- T
 mig.all[mig.all>0] <- T
 mig.all[mig.all==0] <- F
 mig.all <- mig.all * mask.mig.duration
-plot(dates, apply(mig.all, 2, sum, na.rm=T), type='l',
+# Months 1-126 correspond to February 1997 - June 2007.
+mig.all.ts <- ts(apply(mig.all, 2, sum, na.rm=T), start=c(1997, 2), deltat=1/12)
+plot(mig.all.ts, type='l',
     main=paste("All migrations (months=", MONTHS.AWAY, ")", sep=""),
     xlab="Year")
 
@@ -98,10 +89,12 @@ mig.dist <- mig.dist * mask.mig.duration
 mig.dist[mig.dist==1] <- "DL"
 mig.dist[mig.dist==-1] <- "LD"
 mig.dist[mig.dist==0] <- F
-plot(dates, apply(mig.dist=="DL", 2, sum, na.rm=T), type='l',
+mig.DL.ts <- ts(apply(mig.dist=="DL", 2, sum, na.rm=T), start=c(1997, 2), deltat=1/12)
+plot(mig.DL.ts, type='l',
     main=paste("Distant-local migrations (months=", MONTHS.AWAY, ")",
     sep=""), xlab="Year")
-plot(dates, apply(mig.dist=="LD", 2, sum, na.rm=T), type='l',
+mig.LD.ts <- ts(apply(mig.dist=="LD", 2, sum, na.rm=T), start=c(1997, 2), deltat=1/12)
+plot(mig.LD.ts, type='l',
     main=paste("Local-distant migrations (months=", MONTHS.AWAY, ")", sep=""), xlab="Year")
 
 # Now calculate local->local migrations only
@@ -112,8 +105,12 @@ mig.local <- places.local[place1.cols] - places.local[place0.cols]
 mig.local <- mig.local * mask.mig.duration
 mig.local[mig.local!=0] <- "LL"
 mig.local[mig.local==0] <- F
-plot(dates, apply(mig.local=="LL", 2, sum, na.rm=T), type='l',
+mig.LL.ts <- ts(apply(mig.local=="LL", 2, sum, na.rm=T), start=c(1997, 2), deltat=1/12)
+plot(mig.LL.ts, type='l',
     main=paste("Local-local migrations (months=", MONTHS.AWAY, ")", sep=""),
     xlab="Year")
 
 dev.off()
+
+rm(hhreg)
+save.image(file=paste("migration_count-", MONTHS.AWAY, "_months_away.Rdata", sep=""))
