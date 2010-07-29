97c6cc969442358af284c9a9ed4d17c9b2e9285d
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2010-07-29 12:37:30 -0700

    Fix bugs in data loading, clean up plots.

--- OLSLandUse/birth_rates.R
+++ OLSLandUse/birth_rates.R
@@ -1,37 +1,22 @@
 ###############################################################################
-# This calculates an OLS model land use, broken into 3 categories, based on the 
-# calculations in Table 5 of Axinn and Ghimire, PSC Research Report 07-617 
-# (2007). It uses several household and neighborhood level predictors.
-#
-# LULC categories:
-# 	Buildings and infrastructure
-# 	Agricultural land area
-# 	Non-agricultural land area
+# Uses the household registry data to calculate crude birth rates to be used in 
+# an OLS model predicting LULC (along the lines of Axinn and Ghimire, 2007)
 
-library("foreign")
-t1.hhag <- read.xport("/media/Local_Secure/ICPSR_0538_Restricted/da04538-0002_REST.xpt")
-t1.indivcensus <- read.xport("/media/Local_Secure/ICPSR_0538_Restricted/da04538-0004_REST.xpt")
+require(ggplot2)
+require(foreign)
+
 # Load the hhreg dataframe (household registry data)
-#if (!exists("hhreg.reshaped")) load("/media/Local_Secure/CVFS_HHRe/hhreg_reshaped.Rdata")
-if (!exists("hhreg")) {
-    load("/media/Local_Secure/CVFS_HHReg/hhreg126.Rdata")
+load("/media/Local_Secure/CVFS_HHReg/hhreg126.Rdata")
 
-    # First make an indicator for each time period of the neighborhood
-    hhid.cols <- grep('^hhid[0-9]*$', names(hhreg))
-    nids <- sapply(hhreg[hhid.cols], substr, 1, 3)
-    nids <- apply(nids, 2, as.numeric)
-    nids <- data.frame(nids)
-    names(nids) <- gsub('hhid', 'nid', names(nids))
-    hhreg <- cbind(hhreg, nids)
-    nid.cols <- grep('^nid[0-9]*$', names(hhreg))
-}
-lulc <- read.xport("/media/Local_Secure/ICPSR_SupplementalData/Survey_converted/landuse.xpt")
-lulc <- lulc[order(names(lulc))]
-lulc <- lulc[c(8, 24, 49, 1:7, 9:23, 25:48, 50:62)]
-t1.indivhist <- read.xport("/media/Local_Secure/ICPSR_0538_Restricted/da04538-0014_REST.xpt")
+# First make an indicator for each time period of the neighborhood
+hhid.cols <- grep('^hhid[0-9]*$', names(hhreg))
+nids <- sapply(hhreg[hhid.cols], substr, 1, 3)
+nids <- apply(nids, 2, as.numeric)
+nids <- data.frame(nids)
+names(nids) <- gsub('hhid', 'nid', names(nids))
+hhreg <- cbind(hhreg, nids)
+nid.cols <- grep('^nid[0-9]*$', names(hhreg))
 
-###############################################################################
-# For hhreg:
 # I only want data where PLACE1-PLACE126 (columns 227-273 of the dataframe) is 
 # between 1-151 (only the CVFS sample neighborhoods). The data should then be 
 # aggregated for each column from PLACE1-PLACE126 by this neighborhood ID.  
@@ -50,19 +35,21 @@ t1.indivhist <- read.xport("/media/Local_Secure/ICPSR_0538_Restricted/da04538-00
 
 # For POP only include current HH residents (livng coded as 2 or 4)
 livng.cols <- grep('^livng[0-9]*$', names(hhreg))
-pop <- apply(hhreg[livng.cols]==2, 2, sum, na.rm=TRUE)
 # and for BIRTHS , only include the live births (preg coded as 3)
 preg.cols <- grep('^preg[0-9]*$', names(hhreg))
 place.cols <- grep('^place[0-9]*$', names(hhreg))
-livebirths.place <- apply(hhreg[place.cols] <= 151 & hhreg[preg.cols]==3, 2, sum, na.rm=TRUE)
-# Now aggregate the data by neighborhood for BIRTHS and POP (use tapply to do 
-# this)
-#month <- t(array(1:126, dim=c(126, nrow(hhreg))))
+
+# Make an indicator for if a person is a current resident of Chitwan residing 
+# in a survey household
+in.Chitwan <- hhreg[livng.cols]==2 & hhreg[place.cols]<=151
+
+livebirths.total <- apply(hhreg[preg.cols]==3 & in.Chitwan, 2, sum, na.rm=TRUE)
+pop.total <- apply(in.Chitwan, 2, sum, na.rm=TRUE)
 
 # Now calculate the crude birth rate for each neighborhood, equal to the mean 
 # number of persons in the neighborhood divided by the total
 sum.over.nbh <- function (datacols, factorcols, columnnames) {
-    summatrix <- data.frame(nid=c(1:151), matrix(0, 151, 126))
+    summatrix <- data.frame(nid=sprintf("%03d", c(1:151)), matrix(0, 151, 126))
     names(summatrix) <- c("nid", columnnames)
     for (n in 1:126) {
         sum.by.nbh <- aggregate(datacols[,n], by=list("nid"=factorcols[,n]),
@@ -72,12 +59,40 @@ sum.over.nbh <- function (datacols, factorcols, columnnames) {
     }
     return(summatrix)
 }
-births.nbh <- sum.over.nbh(hhreg[preg.cols]==3 & hhreg[livng.cols]==2,
-        hhreg[nid.cols], names(hhreg[preg.cols]))
-pop.nbh <- sum.over.nbh(hhreg[livng.cols]==2, hhreg[nid.cols],
-        names(hhreg[livng.cols]))
-crude.nbh <- births.nbh
-crude.nbh[2:127] <- crude.nbh[2:127] / (pop.nbh[2:127] / 1000)
 
-pdf()
-plot(birth.nbh
+livebirths.nbh <- sum.over.nbh(hhreg[preg.cols]==3 & in.Chitwan,
+        hhreg[nid.cols], names(hhreg[preg.cols]))
+pop.nbh <- sum.over.nbh(hhreg[livng.cols]==2 & in.Chitwan, hhreg[nid.cols],
+        names(hhreg[livng.cols]))
+
+monthlabels <- seq(as.Date("1997/02/01"), as.Date("2007/07/01"),
+        by="month")
+qplot(monthlabels, apply(pop.nbh[2:127], 2, sum), geom="line", xlab="Year",
+        ylab="Total Population Size")
+ggsave("pop.png", width=8.33, height=5.53, dpi=150)
+
+qplot(monthlabels, apply(livebirths.nbh[2:127], 2, sum), geom="line",
+        xlab="Year", ylab="Total Number of Births (per month)")
+ggsave("births.png", width=8.33, height=5.53, dpi=150)
+
+pop.runavg <- t(apply(pop.nbh[2:127], 1, filter, rep(1/12, 12),
+        method="convolution", sides=2))
+births.runtotal <- t(apply(livebirths.nbh[2:127], 1, filter,  rep(1, 12),
+        method="convolution", sides=2))
+crude.birth.rate.nbh <- births.runtotal / (pop.runavg/1000)
+crude.birth.rate.avg <- apply(crude.birth.rate.nbh, 2, mean, na.rm=T)
+
+qplot(monthlabels, crude.birth.rate.avg, geom="line", xlab="Year",
+        ylab="Crude Birth Rate (per 1000 people per year)")
+ggsave("crude_birth_rate.png", width=8.33, height=5.53, dpi=150)
+
+# Output a crude birth rate calculated from Feb 1997 - Jan 2002 to use for my 
+# LULC OLS statistics
+CBR.nbh <- cbind(livebirths.nbh[1], CBR=apply(livebirths.nbh[2:61], 1, sum,
+        na.rm=T) / (apply(pop.nbh[,2:61], 1, mean, na.rm=T)/1000))
+save(CBR.nbh, file="CBH_nbh.Rdata")
+
+# Check results based on the data used in the Axinn 2007, paper. Mean should be 
+# 72.61 births (per thousand people)
+axinn.CBR <- cbind(livebirths.nbh[1], CBR=apply(livebirths.nbh[2:48], 1, sum,
+        na.rm=T) / (apply(pop.nbh[,2:48], 1, mean, na.rm=T)/1000))
