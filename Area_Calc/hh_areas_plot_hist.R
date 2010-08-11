c91aeffbe35c57e2fb09eceb69e33ae411285535
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2010-08-10 23:04:31 -0400

    Add code to write prob dist to text.

--- Area_Calc/hh_areas_plot_hist.R
+++ Area_Calc/hh_areas_plot_hist.R
@@ -7,6 +7,14 @@ theme_update(theme_grey(base_size=18))
 update_geom_defaults("line", aes(size=1))
 update_geom_defaults("step", aes(size=1))
 
+make.txtprob <- function(probs, binlims, param.name) {
+    # param.name is the name used by the ChitwanABM model for this parameter.
+    binlims <- paste(round(binlims, digits=4), collapse=", ")
+    probs <- paste(round(probs, digits=4), collapse=", ")
+    txtprob <- paste("'", param.name, "' : [((", binlims, "), (", probs,  "))] | validate_prob_dist]", sep="")
+    return(txtprob)
+}
+
 hh.areas <- read.csv("hh_areas_T1_sq_meters.csv")
 
 qplot(hh.areas$area, geom="histogram",
@@ -17,9 +25,6 @@ qplot(hh.areas$area, geom="histogram", xlim=c(0,10000),
         xlab="Household Area (square meters)", ylab="Count", binwidth=300)
 ggsave("hh.areas_hist_cut.png", width=8.33, height=5.53, dpi=300)
 
-# Calculate parameters for a gamma distribution to model the data
-fitdistr(hh.areas$area, "gamma", list(shape = 7, rate = 0.001),
-        lower = 0.001)
 hh.areas.binned <- bin(hh.areas$area, binwidth=300)
 lim.upper <- ceiling(hh.areas.binned$x[2:26])
 lim.upper[25] <- 10000
@@ -33,6 +38,8 @@ hh.areas.prob$prob <- hh.areas.prob$prob/sum(hh.areas.prob$prob)
 qplot(lim.upper, prob*100, geom="step", xlab="Household Area (square meters)",
         ylab="Probability (%)", data=hh.areas.prob)
 ggsave("hh.areas_hist_prob.png", width=8.33, height=5.53, dpi=300)
+
+write(make.txtprob(hh.areas.prob$prob, c(hh.areas.prob$lim.upper, 12000), "lulc.area.hh"), file="lulc.area.hh.txt")
 
 print(paste("Mean:", mean(hh.areas$area)))
 print(paste("SD:", sd(hh.areas$area)))
