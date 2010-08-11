e959a6d10699f3f72727c5fef618bb5fb079ef84
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2010-08-10 23:04:58 -0400

    Add code to hhsize to account for place.

--- HHSize/hhsize.R
+++ HHSize/hhsize.R
@@ -3,11 +3,12 @@
 ###############################################################################
 
 library("Hmisc")
-load("/media/Secure_Data/CVFS_HHReg/hhreg126.Rdata")
+load("/media/Local_Secure/CVFS_HHReg/hhreg126.Rdata")
 
 hhreg <- cbind(hhreg, migrated=0)
 
 livng.cols <- grep('^livng[0-9]*$', names(hhreg))
+place.cols <- grep('^place[0-9]*$', names(hhreg))
 hhid.cols <- grep('^hhid[0-9]*$', names(hhreg))
 
 # Recode livng vars so that they are presense/absence of each indiv in the 
@@ -18,17 +19,30 @@ hhreg[livng.cols][hhreg[livng.cols] == 4] <- 1
 hhreg[livng.cols][hhreg[livng.cols] == 5] <- 1
 hhreg[livng.cols][hhreg[livng.cols] != 1] <- 0
 
-livngs.data <- cbind(hhreg[livng.cols], hhreg[hhid.cols])
+# Recode place.cols to code whether a person was resident within a sample 
+# neighborhood (1) or not (0).
+hhreg[place.cols] <- apply(hhreg[,place.cols], 2, as.character)
+hhreg[place.cols] <- apply(hhreg[,place.cols], 2, as.numeric)
+hhreg[place.cols][hhreg[place.cols] > 151] <- 0
+hhreg[place.cols][hhreg[place.cols] < 1] <- 0
+hhreg[place.cols][(hhreg[place.cols] >= 1) & (hhreg[place.cols] <= 151)] <- 1
+
+livngs.data <- cbind(hhreg[hhid.cols], hhreg[livng.cols], hhreg[place.cols])
 
 hhsize.mean = c()
 numhs = c()
 pop = c()
 for (colnum in 1:126) {
     livng.col <- grep(paste('^livng', colnum, '$', sep=''), names(livngs.data))
+    place.col <- grep(paste('^place', colnum, '$', sep=''), names(livngs.data))
     hhid.col <- grep(paste('^hhid', colnum, '$', sep=''), names(livngs.data))
 
     hhsizes.new <- aggregate(livngs.data[,livng.col],
             by=list(livngs.data[,hhid.col]), sum)
+    # Ask Dirgha how to do this - use just livng, just place, or livng and 
+    # place?
+    #hhsizes.new <- aggregate(livngs.data[,livng.col] & livngs.data[,place.col],
+    #        by=list(livngs.data[,hhid.col]), sum)
 
     hhsize.mean <- c(hhsize.mean, mean(hhsizes.new$x, na.rm=TRUE))
     numhs <- c(numhs, length(hhsizes.new$x[!is.na(hhsizes.new$x)]))
