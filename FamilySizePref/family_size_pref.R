590a260148499c9c685ebb1dc1a59b1bd1926333
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2010-10-11 11:25:06 -0700

    Fix problem with "god's will" response.

--- FamilySizePref/family_size_pref.R
+++ FamilySizePref/family_size_pref.R
@@ -23,19 +23,18 @@ names(desnumchild)[3] <- "numchild"
 # People who said "it is god's will" were coded as 97, and reasked the 
 # question, in f9.
 godswill <- which(desnumchild$numchild==97)
-desnumchild[godswill,]$numchild <- desnumchild$f9[godswill]
+desnumchild[godswill,]$numchild <- t1indiv$f9[godswill]
+# Some still have "god's will", so recode these as -3 (don't know)
+desnumchild$numchild[desnumchild$numchild==97] <- -3
 # 2 people said a range from low to high. Here, arbitrarily, take the high 
 # number, stored in f7b.
 range <- which(desnumchild$numchild==95)
-desnumchild[range,]$numchild <- desnumchild$f7b[range]
+desnumchild[range,]$numchild <- t1indiv$f7b[range]
 # 28 people said they don't know. This is coded as -3 in the CVFS data. Recode 
 # this as -1.
 desnumchild$numchild[desnumchild$numchild==-3] <- -1
 # Also recode no response given (NA in the dataset) as -1
 desnumchild$numchild[is.na(desnumchild$numchild)] <- -1
-# TODO: Also there are 22 individuals with # kids wanted in the thousands...  
-# ask Dirgha what these are
-desnumchild$numchild[desnumchild$numchild>1000] <- -1
 save(desnumchild, file="desnumchild.Rdata")
 
 # Make a distribution to use in the model
