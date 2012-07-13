4b9b49845e0a388a0f17ee8de255a39daec35561
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2012-07-12 22:10:49 -0700

    Integrate old shell script into main python script for household area calculation.

--- Area_Calc/hh_areas_1_preprocess.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm hh_areas.txt
-touch hh_areas.txt
-for file in `grep -cRlE "HOUSEHOLD|household" /media/Local_Secure/CVFS_Land_Use_Mapping/T1`; do grep Acre $file >> hh_areas.txt; done
