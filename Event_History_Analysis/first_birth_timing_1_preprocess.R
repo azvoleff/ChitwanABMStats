bcd3aca01c71be01aa5c100b7e40a516e71ac289
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2012-07-23 21:09:54 -0700

    Continue first birth timing work.

--- Event_History_Analysis/first_birth_timing_1_preprocess.R
+++ Event_History_Analysis/first_birth_timing_1_preprocess.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 ###############################################################################
-# Encodes marriage data, as right censored data. Only includes individuals 
+# Encodes first_birth data, as right censored data. Only includes individuals 
 # present in 1996. Encodes wide and long format person-month dataset for later 
 # analysis with glmer and or MLwiN.
 # 
@@ -13,14 +13,14 @@
 ###############################################################################
 
 # Hmisc is needed as hhreg is a "labelled" dataframe. If Hmisc is not included, 
-# will get errors saying "cannot coerce class "labelled" into a data.frame"
+# will get errors saying "cannot coerce class "labelled" into a data.frame".
 library(Hmisc)
 library(ggplot2)
 library(foreign)
 
 # Months.total is how many months of the household registry to include (max 
 # number of months is 126, so to include all the months set LAST_MONTH to 126).
-LAST_MONTH <- 90 # Yabiku (2006) uses 90 months
+LAST_MONTH <- 60
 
 print("Loading data...")
 load("V:/Nepal/CVFS_HHReg/hhreg126.Rdata")
@@ -31,13 +31,14 @@ if (LAST_MONTH < max(varying_cols_times)) {
     drop_cols <- varying_cols[varying_cols_times > LAST_MONTH]
     hhreg <- hhreg[-drop_cols]
 }
-
-preg_cols <- grep('^preg[0-9]*$', names(hhreg))
 hhid_cols <- grep('^hhid[0-9]*$', names(hhreg))
 marit_cols <- grep('^(marit)[0-9]*$', names(hhreg))
 place_cols <- grep('^(place)[0-9]*$', names(hhreg))
 age_cols <- grep('^age[0-9]*$', names(hhreg))
 
+###############################################################################
+# Add interpolated log(agricultural vegetation) columns
+###############################################################################
 # Load the LULC data:
 lu <- read.xport("V:/Nepal/ICPSR_SupplementalData/Survey_converted/landuse.xpt")
 land_agveg_t1 <- with(lu, rowSums(cbind(BARI1, IKHET1, RKHET1)))
@@ -84,17 +85,27 @@ interp_logpercagveg <- log(interp_percagveg + 1)
 interp_logpercagveg <- data.frame(NEIGHID=lu_t2$NEIGHID, interp_logpercagveg)
 names(interp_logpercagveg)[2:ncol(interp_logpercagveg)] <- paste("logpercagveg", seq(1:LAST_MONTH), sep="")
 
-# Load the life history calendar data to see if women/spouses have ever been 
-# sterilized.
+###############################################################################
+# Process LHC
+###############################################################################
 lhc <- read.xport('M:/Data/Nepal/CVFS_Public/20120722_Chitwan_Unrestricted_Data/ICPSR_04538/DS0013/04538_0013_data.xpt')
-lhc_vars <- with(lhc, data.frame(RESPID, ever_ster=BCYN8, sp_ever_ster=BCYN9))
+lhc_marr_cols <- grep('^(M1E199[4-9])|(M1E20[0-5][0-9])$',names(lhc))
+marr_year <- apply(lhc[lhc_marr_cols], 1,
+                    function(marit_row) match(1, marit_row))
+mths_marr_pre_1997 <- (length(lhc_marr_cols) - marr_year) * 12 + (13 - lhc$MARR1MTH)
+mths_marr_pre_1997[is.na(mths_marr_pre_1997)] <- 0
+lhc_age_cols <- grep('^(M1E199[4-9])|(M1E20[0-5][0-9])$',names(lhc))
+marr_age_pre97_marriages <- lhc$AGE2053 - (length(lhc_marr_cols) - marr_year)
+# The BCYN variables are whether respondent and respondent's spouse have ever 
+# been sterilized.
+lhc_vars <- with(lhc, data.frame(RESPID, ever_ster=BCYN8, sp_ever_ster=BCYN9,
+                                 spouse_2=MYN2, marr_age_pre97_marriages))
 
 t1indiv <- read.xport("V:/Nepal/ICPSR_0538_Restricted/da04538-0012_REST.xpt")
 t1indiv <- merge(t1indiv, lhc_vars)
-
 # To merge with the hhreg data, need to convert the old format respondent ID 
-                 # (NNNHHSS) (where NNN is NBH ID, HH is household ID, and SS 
-                 # is subject ID) to NNNHHHSSS:
+# (NNNHHSS) (where NNN is NBH ID, HH is household ID, and SS is subject ID) to 
+# NNNHHHSSS:
 old_respID <- sprintf("%07i", t1indiv$RESPID)
 NBHID <- sprintf("%03i", as.numeric(substr(old_respID, 1, 3)))
 HHID <- sprintf("%03i", as.numeric(substr(old_respID, 4, 5)))
@@ -102,15 +113,16 @@ SUBJID <- sprintf("%03i", as.numeric(substr(old_respID, 6, 7)))
 t1indiv$RESPID <- paste(NBHID, HHID, SUBJID, sep="")
 t1indiv_vars <- with(t1indiv, data.frame(respid=RESPID, schooling_yrs=A1, 
                                          had_children=C2, ever_ster, 
-                                         sp_ever_ster))
-
+                                         sp_ever_ster, mths_marr_pre_1997, 
+                                         spouse_2, marr_age_pre97_marriages))
 hhreg <- merge(hhreg, t1indiv_vars, all.x=TRUE)
+
 # Code "had children" and ever sterilized variables as -1 for people who were 
 # not interviewed in 1996, so new respondents who were not interviewed in 1996 
 # won't be excluded from the data.
-hhreg$had_children[hhreg$individl != 1] <- -1
-hhreg$ever_ster[hhreg$individl != 1] <- -1
-hhreg$sp_ever_ster[hhreg$individl != 1] <- -1
+#hhreg$had_children[hhreg$individl != 1] <- -1
+#hhreg$ever_ster[hhreg$individl != 1] <- -1
+#hhreg$sp_ever_ster[hhreg$individl != 1] <- -1
 
 hhreg$gender <- factor(hhreg$gender, labels=c("male", "female"))
 hhreg$ethnic <- factor(hhreg$ethnic, levels=c(1,2,3,4,5,6), labels=c("UpHindu",
@@ -136,12 +148,12 @@ hhreg[marit_cols][hhreg[marit_cols] == 6] <- NA
 # Following Ghimire and Hoelter (2007), the sample is all women "aged 15–29 in 
 # 1996 who had not previously given birth and who were neither sterilized nor 
 # married to men who were sterilized". Also ensure that women were local in 
-# 1996.
+# 1996, and had not have been married more than once as of 1996
 in_sample <- (hhreg[age_cols[1]] >=15)     & (hhreg[age_cols[1]] <= 29) &
              (hhreg[place_cols[1]] > 1)    & (hhreg[place_cols[1]] <= 151) &
              !is.na(hhreg[, hhid_cols[1]]) & (hhreg$gender == "female") &
-             (hhreg$had_children != 1)  & (hhreg$sp_ever_ster != 1) &
-             (hhreg$ever_ster != 1)
+             (hhreg$had_children != 1)     & (hhreg$sp_ever_ster != 1) &
+             (hhreg$ever_ster != 1)        & (hhreg$spouse_2 == 0)
 table(in_sample, exclude=NULL)
 #table(hhreg$had_children, exclude=NULL)
 #table(hhreg$sp_ever_ster, exclude=NULL)
@@ -152,7 +164,7 @@ sample <- hhreg[in_sample, ]
 # Save the variables that will be needed later as independent variables, 
 # including the neighborhood and household IDs, for each person, and some other 
 # covariates.
-indepvars <- cbind(respid=hhreg$respid, hhreg[hhid_cols], hhreg[place_cols], ethnic=hhreg$ethnic, gender=hhreg$gender, hhreg[age_cols], originalHH=hhreg$hhid1, originalNBH=hhreg$place1, schooling_yrs=hhreg$schooling_yrs)
+indepvars <- cbind(respid=hhreg$respid, hhreg[hhid_cols], hhreg[place_cols], ethnic=hhreg$ethnic, gender=hhreg$gender, hhreg[age_cols], originalHH=hhreg$hhid1, originalNBH=hhreg$place1, schooling_yrs=hhreg$schooling_yrs, mths_marr_pre_1997=hhreg$mths_marr_pre_1997)
 indepvars <- indepvars[in_sample,]
 indepvars <- merge(indepvars, interp_logpercagveg, by.x="originalNBH", by.y="NEIGHID", all.x=TRUE)
 
@@ -163,27 +175,31 @@ indepvars <- merge(indepvars, interp_logpercagveg, by.x="originalNBH", by.y="NEI
 # setting every cell in the row after that one to NA. Also censor every cell in 
 # a row after the first NA in that row.
 print("Censoring data...")
-censor_data <- function(first_marr_column, record) {
-    first_birth_column <- na.omit(match(1, record))
+censor_data <- function(first_marr_col, last_marr_col, preg_record) {
+    first_birth_column <- na.omit(match(1, preg_record))
     if (length(first_birth_column) > 0) {
         first_birth_column <- min(first_birth_column[!is.na(first_birth_column)])
     } else first_birth_column <- NA
-    first_NA_column <- match(TRUE, is.na(record))
-    # First censor all observations up until the first marriage
-    if (is.na(first_marr_column) | (first_marr_column > (length(record) - 8))) {
+    first_NA_column <- match(TRUE, is.na(preg_record))
+    # First censor all observations up until the first marriage, and censor 
+    # after dissolution of first marriage
+    if (is.na(first_marr_col) | (first_marr_col > (length(preg_record) - 8))) {
         # If respondent never married, or married within the last 8 months of 
-        # data collection, set whole record to NA:
-        return(rep(NA, length(record)))
+        # data collection, set whole preg_record to NA:
+        return(rep(NA, length(preg_record)))
     }
-    else if (first_marr_column != 1) {
-        record[1:(first_marr_column - 1)] <- NA
+    else if (first_marr_col != 1) {
+        preg_record[1:(first_marr_col - 1)] <- NA
+    }
+    if (!is.na(last_marr_col) && last_marr_col < length(preg_record)) {
+        preg_record[last_marr_col:length(preg_record)] <- NA
     }
     # Now censor all observations after 1st pregnancy resulting in a live 
     # birth, or after first NA column in the pregnancy data. First pregnancy is 
     # defined as the month 9 months prior to the first live birth.  The 
     # min(!is.na below is necessary to find a match of any type of live birth.
     if (is.na(first_birth_column) && is.na(first_NA_column)) {
-        return(record)
+        return(preg_record)
     } else if (is.na(first_birth_column)) {
         first_censored_col <- first_NA_column
     } else if (is.na(first_NA_column)) {
@@ -194,41 +210,59 @@ censor_data <- function(first_marr_column, record) {
         first_censored_col <- first_birth_column - 8
     }
     if (first_censored_col >= 1) {
-        record[first_censored_col:length(record)] <- NA
+        preg_record[first_censored_col:length(preg_record)] <- NA
     }
     if (!is.na(first_birth_column) && ((first_birth_column - 8) >= 1)) {
-        record[first_birth_column - 8] <- 1
+        preg_record[first_birth_column - 8] <- 1
     }
-    return(record)
+    return(preg_record)
 }
 sample_preg_cols <- grep('^(preg)[0-9]*$', names(sample))
 sample_marr_cols <- grep('^(marit)[0-9]*$', names(sample))
-marr_month <- apply(sample[sample_marr_cols], 1,
+first_marr_col <- apply(sample[sample_marr_cols], 1,
                     function(marit_row) match(1, marit_row))
+last_marr_col <- apply(sample[sample_marr_cols], 1,
+    function(marit_row) (length(marit_row) - match(1, rev(marit_row)) + 1))
 live_births <- sample[sample_preg_cols]
 live_births <- (live_births == 3) | (live_births == 5)
-first_births <- t(mapply(censor_data, marr_month, 
-                             as.list(as.data.frame(t(live_births)))))
+first_births <- t(mapply(censor_data, first_marr_col, last_marr_col,
+                         as.list(as.data.frame(t(live_births)))))
 # Apply returned a matrix, and lost its row and column names.  Reassign them so 
 # first_births ends up as the final censored matrix with correct row and column 
 # names.
-first_births <- data.frame(first_births, row.names=row.names(sample))
+first_births <- data.frame(first_births)
 names(first_births) <- sub('preg', 'first_birth', names(sample[sample_preg_cols]))
-first_births$respid=row.names(first_births)
+first_births$respid <- sample$respid
+
+# Add a time-varying number of months married variable, remembering that we 
+# need to add additional months for women married pre-1997
+n_months_marr <- sample[sample_marr_cols]
+n_months_marr[, 1] <- n_months_marr[, 1] + sample$mths_marr_pre_1997
+n_months_marr <- as.data.frame(t(apply(n_months_marr, 1, cumsum)))
+names(n_months_marr) <- paste('n_months_marr', seq(1, length(sample_marr_cols)), sep="")
+n_months_marr$respid <- sample$respid
+indepvars <- merge(indepvars, n_months_marr)
+
+# Add age at first-marriage variable:
+sample_age_cols <- grep('^(age)[0-9]*$', names(sample))
+indepvars$age_at_first_marr <- mapply(function(age_row, marr_col) age_row[marr_col],
+                        as.list(as.data.frame(t(sample[sample_age_cols]))),
+                        first_marr_col)
+pre_97_rows <- !is.na(sample$marr_age_pre97_marriages)
+indepvars$age_at_first_marr[pre_97_rows] <- sample$marr_age_pre97_marriages[pre_97_rows]
 
 ###############################################################################
 # Output the data
 ###############################################################################
 print("Outputting censored data...")
-
 # First output in wide format
 # Add columns with neighborhood and household ID, ethnicity, age, sex, and 
 # hhid.
 first_births_wide <- merge(indepvars, first_births, by="respid", all.x=F, all.y=T)
 # Need to order the data properly for it to be used in MLwiN
 first_births_wide <- first_births_wide[order(first_births_wide$respid, first_births_wide$originalHH, first_births_wide$originalNBH),]
-save(first_births_wide, file=paste("data/marriage_data-wideformat-up_to_month_", LAST_MONTH, ".Rdata", sep=""))
-write.csv(first_births_wide, file=paste("data/marriage_data-wideformat-up_to_month_", LAST_MONTH, ".csv", sep=""), row.names=FALSE)
+save(first_births_wide, file=paste("data/first_birth_data-wideformat-up_to_month_", LAST_MONTH, ".Rdata", sep=""))
+write.csv(first_births_wide, file=paste("data/first_birth_data-wideformat-up_to_month_", LAST_MONTH, ".csv", sep=""), row.names=FALSE)
 
 # Now in long format
 first_births_cols <- grep('^first_birth[0-9]*$', names(first_births_wide))
@@ -236,15 +270,17 @@ age_cols <- grep('^age[0-9]*$', names(first_births_wide))
 hhid_cols <- grep('^hhid[0-9]*$', names(first_births_wide))
 logpercagveg_cols <- grep('^logpercagveg[0-9]*$', names(first_births_wide))
 place_cols <- grep('^place[0-9]*$', names(first_births_wide))
+n_months_marr_cols <- grep('^n_months_marr[0-9]*$', names(first_births_wide))
 # Now construct the long-format dataset
 first_births_long <- reshape(first_births_wide, idvar="respid", 
                              varying=list(first_births_cols, age_cols,
                                           hhid_cols, place_cols, 
-                                          logpercagveg_cols), 
-                      v.names=c("first_birth", "age", "hhid", "place", 
-                                "logpercagveg"),
+                                          logpercagveg_cols, 
+                                          n_months_marr_cols), 
+                             v.names=c("first_birth", "age", "hhid", "place", 
+                                "logpercagveg", "n_months_marr"),
                              direction="long", sep="")
 first_births_long <- first_births_long[!is.na(first_births_long$first_birth),]
 first_births_long <- first_births_long[order(first_births_long$respid, first_births_long$originalHH, first_births_long$originalNBH),]
-save(first_births_long, file=paste("data/marriage_data-longformat-up_to_month_", LAST_MONTH, ".Rdata", sep=""))
-write.csv(first_births_long, file=paste("data/marriage_data-longformat-up_to_month_", LAST_MONTH, ".csv", sep=""), row.names=FALSE)
+save(first_births_long, file=paste("data/first_birth_data-longformat-up_to_month_", LAST_MONTH, ".Rdata", sep=""))
+write.csv(first_births_long, file=paste("data/first_birth_data-longformat-up_to_month_", LAST_MONTH, ".csv", sep=""), row.names=FALSE)
