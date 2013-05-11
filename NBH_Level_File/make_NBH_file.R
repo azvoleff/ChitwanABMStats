107bd9805d928149c1f2232b66a58c2f7780bbc6
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2013-05-11 10:06:33 -0700

    Update nbh level recode scripts.

--- NBH_Level_File/make_NBH_file.R
+++ NBH_Level_File/make_NBH_file.R
@@ -6,7 +6,7 @@ nbhhist <- read.xport("W:/Nepal/ICPSR_0538_Restricted/da04538-0014_REST.xpt")
 
 ID_cols <- grep('^(NEIGHID|STRATA|NX|NY)$', names(nbhhist))
 nbh_recode <- nbhhist[ID_cols]
-nbh_recode$elec_avail <- nbhhist$ELEC52
+nbh_recode$ELEC_1996 <- nbhhist$ELEC52
 nbh_recode$NEIGHID <- factor(sprintf("%03d", nbh_recode$NEIGHID))
 nbh_recode$STRATA <- factor(nbh_recode$STRATA)
 
@@ -14,8 +14,8 @@ nbh_recode$STRATA <- factor(nbh_recode$STRATA)
 # been available in the neighborhood (for consistency with established models).
 nbh_serv_cols <- grep('^(SCHLFT|HLTHFT|BUSFT|MARFT|EMPFT)[1-5][0-9]$', 
                       names(nbhhist))
-nbh_recode$avg_yrs_services_lt15 <- rowSums(nbhhist[nbh_serv_cols] <= 15 ) / 5
-nbh_recode$avg_yrs_services_lt30 <- rowSums(nbhhist[nbh_serv_cols] <= 30 ) / 5
+nbh_recode$avg_yrs_services_lt15_1996 <- rowSums(nbhhist[nbh_serv_cols] <= 15 ) / 5
+nbh_recode$avg_yrs_services_lt30_1996  <- rowSums(nbhhist[nbh_serv_cols] <= 30 ) / 5
 
 # Merge the 1996 non-family services data.  Below are the variables for 
 # non-family services w/in a 1 hour walk at age 12.
@@ -37,41 +37,41 @@ nbh_recode$dist_nara <- (sqrt((nbh_recode$NX - 245848)**2 + (nbh_recode$NY - 306
 # Load and recode the LULC data
 lulc_orig <- read.xport("W:/Nepal/ICPSR_SupplementalData/Survey_converted/landuse.xpt")
 
-agveg_t1 <- with(lulc_orig, rowSums(cbind(BARI1, IKHET1, RKHET1)))
-nonagveg_t1 <- with(lulc_orig, rowSums(cbind(GRASSC1, GRASSP1, PLANTC1, PLANTP1)))
-privbldg_t1 <- with(lulc_orig, rowSums(cbind(HHRESID1, MILL1, OTRBLD1)))
-pubbldg_t1 <- with(lulc_orig, rowSums(cbind(ROAD1, SCHOOL1, TEMPLE1)))
-other_t1 <- with(lulc_orig, rowSums(cbind(CANAL1, POND1, RIVER1, SILT1, UNDVP1)))
+agveg_1996 <- with(lulc_orig, rowSums(cbind(BARI1, IKHET1, RKHET1)))
+nonagveg_1996 <- with(lulc_orig, rowSums(cbind(GRASSC1, GRASSP1, PLANTC1, PLANTP1)))
+privbldg_1996 <- with(lulc_orig, rowSums(cbind(HHRESID1, MILL1, OTRBLD1)))
+pubbldg_1996 <- with(lulc_orig, rowSums(cbind(ROAD1, SCHOOL1, TEMPLE1)))
+other_1996 <- with(lulc_orig, rowSums(cbind(CANAL1, POND1, RIVER1, SILT1, UNDVP1)))
 
-agveg_t2 <- with(lulc_orig, rowSums(cbind(BARI2, IKHET2, RKHET2)))
-nonagveg_t2 <- with(lulc_orig, rowSums(cbind(GRASSC2, GRASSP2, PLANTC2, PLANTP2)))
-privbldg_t2 <- with(lulc_orig, rowSums(cbind(HHRESID2, MILL2, OTRBLD2)))
-pubbldg_t2 <- with(lulc_orig, rowSums(cbind(ROAD2, SCHOOL2, TEMPLE2)))
-other_t2 <- with(lulc_orig, rowSums(cbind(CANAL2, POND2, RIVER2, SILT2, UNDVP2)))
+agveg_2000 <- with(lulc_orig, rowSums(cbind(BARI2, IKHET2, RKHET2)))
+nonagveg_2000 <- with(lulc_orig, rowSums(cbind(GRASSC2, GRASSP2, PLANTC2, PLANTP2)))
+privbldg_2000 <- with(lulc_orig, rowSums(cbind(HHRESID2, MILL2, OTRBLD2)))
+pubbldg_2000 <- with(lulc_orig, rowSums(cbind(ROAD2, SCHOOL2, TEMPLE2)))
+other_2000 <- with(lulc_orig, rowSums(cbind(CANAL2, POND2, RIVER2, SILT2, UNDVP2)))
 
-agveg_t3 <- with(lulc_orig, rowSums(cbind(BARI3, IKHET3, RKHET3)))
-nonagveg_t3 <- with(lulc_orig, rowSums(cbind(GRASSC3, GRASSP3, PLANTC3, PLANTP3)))
-privbldg_t3 <- with(lulc_orig, rowSums(cbind(HHRESID3, MILL3, OTRBLD3)))
-pubbldg_t3 <- with(lulc_orig, rowSums(cbind(ROAD3, SCHOOL3, TEMPLE3)))
-other_t3 <- with(lulc_orig, rowSums(cbind(CANAL3, POND3, RIVER3, SILT3, UNDVP3)))
+agveg_2007 <- with(lulc_orig, rowSums(cbind(BARI3, IKHET3, RKHET3)))
+nonagveg_2007 <- with(lulc_orig, rowSums(cbind(GRASSC3, GRASSP3, PLANTC3, PLANTP3)))
+privbldg_2007 <- with(lulc_orig, rowSums(cbind(HHRESID3, MILL3, OTRBLD3)))
+pubbldg_2007 <- with(lulc_orig, rowSums(cbind(ROAD3, SCHOOL3, TEMPLE3)))
+other_2007 <- with(lulc_orig, rowSums(cbind(CANAL3, POND3, RIVER3, SILT3, UNDVP3)))
 
-lulc <- data.frame(NEIGHID=lulc_orig$NEIGHID, agveg_t1, nonagveg_t1, privbldg_t1, 
-                   pubbldg_t1, other_t1, agveg_t2, nonagveg_t2, privbldg_t2, 
-                   pubbldg_t2, other_t2, agveg_t3, nonagveg_t3, privbldg_t3, 
-                   pubbldg_t3, other_t3)
+lulc <- data.frame(NEIGHID=lulc_orig$NEIGHID, agveg_1996, nonagveg_1996, privbldg_1996, 
+                   pubbldg_1996, other_1996, agveg_2000, nonagveg_2000, privbldg_2000, 
+                   pubbldg_2000, other_2000, agveg_2007, nonagveg_2007, privbldg_2007, 
+                   pubbldg_2007, other_2007)
 # Convert land areas expressed in square feet to square meters
 lulc[2:ncol(lulc)]  <- lulc[2:ncol(lulc)] * .09290304
 
-lulc$total_t1 <- apply(lulc[grep('_t1$', names(lulc))], 1, sum)
-lulc$total_t2 <- apply(lulc[grep('_t2$', names(lulc))], 1, sum)
-lulc$total_t3 <- apply(lulc[grep('_t3$', names(lulc))], 1, sum)
+lulc$total_1996 <- apply(lulc[grep('_1996$', names(lulc))], 1, sum)
+lulc$total_2000 <- apply(lulc[grep('_2000$', names(lulc))], 1, sum)
+lulc$total_2007 <- apply(lulc[grep('_2007$', names(lulc))], 1, sum)
 
-lulc$percagveg_t1 <- with(lulc, (agveg_t1/total_t1)*100)
-lulc$percagveg_t2 <- with(lulc, (agveg_t2/total_t2)*100)
-lulc$percagveg_t3 <- with(lulc, (agveg_t3/total_t3)*100)
-lulc$logpercagveg_t1 <- log(lulc$percagveg_t1 + 1)
-lulc$logpercagveg_t2 <- log(lulc$percagveg_t2 + 1)
-lulc$logpercagveg_t3 <- log(lulc$percagveg_t3 + 1)
+lulc$percagveg_1996 <- with(lulc, (agveg_1996/total_1996)*100)
+lulc$percagveg_2000 <- with(lulc, (agveg_2000/total_2000)*100)
+lulc$percagveg_2007 <- with(lulc, (agveg_2007/total_2007)*100)
+lulc$logpercagveg_1996 <- log(lulc$percagveg_1996 + 1)
+lulc$logpercagveg_2000 <- log(lulc$percagveg_2000 + 1)
+lulc$logpercagveg_2007 <- log(lulc$percagveg_2007 + 1)
 
 nbh_recode <- merge(nbh_recode, lulc)
 
--- NBH_Level_File/make_NBH_file_t2.R
+++ NBH_Level_File/make_NBH_file_t2.R
@@ -2,7 +2,7 @@ library(foreign)
 
 nbht2_all <- read.dta("W:/Nepal/20120704_ICPSR_04538/DS0022/04538-0022-Data-REST.dta")
 
-nbht2 <- nbht2_all$NEIGHID
+nbht2 <- data.frame(NEIGHID=nbht2_all$NEIGHID)
 
 non_fam_58 <- nbht2_all[grep('^(SCHLFT58|HLTHFT58|BUSFT58|EMPFT58|MARFT58)$', 
                          names(nbht2_all))]
@@ -12,5 +12,5 @@ nbht2 <- cbind(nbht2, non_fam_58)
 
 nbht2$ELEC_2001 <- nbht2_all$ELEC58
 
-save(nbh_recode, file="nbh_t2.Rdata")
-write.csv(nbh_recode, file="nbh_t2.csv", row.names=FALSE)
+save(nbht2, file="nbh_t2.Rdata")
+write.csv(nbht2, file="nbh_t2.csv", row.names=FALSE)
